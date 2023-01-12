ebdf04374b2eff353a5228a65e80da5b48e1cd42
Marcelo Saravia <molinito48@gmail.com>
2023-01-12 15:23:40 -0300

    lados

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  port: process.env.PORT || 3000,
+  plugins: [react()]
 })
